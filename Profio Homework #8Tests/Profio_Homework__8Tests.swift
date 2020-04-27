62a42b1aaadd55cc9bb44c38aeba6f6362ce2a8d
Blake Profio <b.profio@setonhill.edu>
2020-04-27 12:17:19 -0400

    Initial Commit

---
+++ Profio Homework #8Tests/Profio_Homework__8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Profio_Homework__8Tests.swift
+//  Profio Homework #8Tests
+//
+//  Created by Blake Profio on 4/27/20.
+//  Copyright © 2020 Blake Profio. All rights reserved.
+//
+
+import XCTest
+@testable import Profio_Homework__8
+
+class Profio_Homework__8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
